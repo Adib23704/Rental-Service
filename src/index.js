5aed81ed9c884cdabfdf08c398b02dfc710993d0
Adib23704 <adib23704@gmail.com>
2024-03-29 14:13:51 +0600

    Added base & frontend base

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.static('./public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+async function main() {
+	app.listen(port, () => {
+		console.log(`Running on port http://localhost:${port}`);
+	});
+}
+
+main();
